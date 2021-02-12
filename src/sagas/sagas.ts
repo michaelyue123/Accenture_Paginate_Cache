3d6a95f3e7f14c3843ca48452d5bb7c7026cbd85
s3796237 <michaelyue123@gmail.com>
2021-02-12 11:25:16 +1100

    initial project setup

---
+++ src/sagas/sagas.ts
@@ -0,0 +1,10 @@
+import { all } from "redux-saga/effects";
+
+
+function* getApiData() {
+    
+}
+
+export default function* rootSaga() {
+    yield all([getApiData]);
+}
---
+++ src/store/configureStore.ts
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
+
+import rootReducer from "../reducers";
+import rootSaga from "../sagas/sagas";
+
+// const sagaMiddleware = createSagaMiddleware();
+
+const configureStore = () => {
+  const store = createStore(rootReducer);
+  
+  return store;
+};
+
+export default configureStore;
